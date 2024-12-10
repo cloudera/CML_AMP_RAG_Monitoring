86f8b4c9b5fba35ff7d706c445af5ff2b5a986e6
Baasit Sharief <bsharief@cloudera.com>
2024-12-10 16:20:53 -0700

    create new dir if it doesn't exist

--- ragmon/main.py
+++ ragmon/main.py
@@ -64,7 +64,9 @@ def start_evaluation_reconciler():
     """Start the evaluation reconciler."""
     logger.info("Starting evaluation reconciler.")
     data_directory = os.path.join(os.getcwd(), "data")
+    logger.info("Data directory: %s", data_directory)
     if not os.path.exists(data_directory):
+        logger.info("Data directory doesn't exxist. Creating data directory.")
         os.makedirs(data_directory)
     logger.info("Data directory: %s", data_directory)
     worker_thread = threading.Thread(
