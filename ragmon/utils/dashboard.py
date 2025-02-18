40096f8deb497ab9562556b25854e90528d5c700
Baasit Sharief <bsharief@cloudera.com>
2025-02-18 11:25:27 -0700

    WIP dashboard reimplementation

--- ragmon/utils/dashboard.py
+++ ragmon/utils/dashboard.py
@@ -266,6 +266,7 @@ def get_numeric_metrics_df(request: MLFlowStoreMetricRequest):
     """
     metric_name = request.metric_names[0]
     metrics_response = get_metrics(request)
+    st.write(metrics_response)
     if metrics_response != []:
         metric_response_ids = [x["experiment_run_id"] for x in metrics_response]
         metric_scores = [
